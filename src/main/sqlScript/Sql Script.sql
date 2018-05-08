3d574c3ac7431329a1e7982467307d4ec3df6e68
Pavel <pavel3423@gmail.com>
2018-05-08 16:31:25 +0300

    update sql script

--- src/main/sqlScript/Sql Script.sql
+++ src/main/sqlScript/Sql Script.sql
@@ -10,35 +10,6 @@ CREATE TABLE IF NOT EXISTS `brands_car` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
--- Дамп структуры для таблица car_catalog.cars
-DROP TABLE IF EXISTS `cars`;
-CREATE TABLE IF NOT EXISTS `cars` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `brandID` int(11) NOT NULL DEFAULT '0',
-  `modelID` int(11) NOT NULL DEFAULT '0',
-  `carClass` varchar(50) NOT NULL DEFAULT '0',
-  `equipment` varchar(50) NOT NULL,
-  `price` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `fk_cars_brand` (`brandID`),
-  KEY `fk_cars_model` (`modelID`),
-  CONSTRAINT `fk_cars_brand` FOREIGN KEY (`brandID`) REFERENCES `brands_car` (`id`),
-  CONSTRAINT `fk_cars_model` FOREIGN KEY (`modelID`) REFERENCES `models_car` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Дамп структуры для таблица car_catalog.favorites
-DROP TABLE IF EXISTS `favorites`;
-CREATE TABLE IF NOT EXISTS `favorites` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` int(11) NOT NULL DEFAULT '0',
-  `carID` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `fk_favorites_user` (`userID`),
-  KEY `fk_favorites_car` (`carID`),
-  CONSTRAINT `fk_favorites_car` FOREIGN KEY (`carID`) REFERENCES `cars` (`id`),
-  CONSTRAINT `fk_favorites_user` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 -- Дамп структуры для таблица car_catalog.models_car
 DROP TABLE IF EXISTS `models_car`;
 CREATE TABLE IF NOT EXISTS `models_car` (
@@ -72,3 +43,32 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `fk_users_roles_idx` (`roleID`),
   CONSTRAINT `fk_users_roles` FOREIGN KEY (`roleID`) REFERENCES `roles` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Дамп структуры для таблица car_catalog.cars
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE IF NOT EXISTS `cars` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `brandID` int(11) NOT NULL DEFAULT '0',
+  `modelID` int(11) NOT NULL DEFAULT '0',
+  `carClass` varchar(50) NOT NULL DEFAULT '0',
+  `equipment` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_cars_brand` (`brandID`),
+  KEY `fk_cars_model` (`modelID`),
+  CONSTRAINT `fk_cars_brand` FOREIGN KEY (`brandID`) REFERENCES `brands_car` (`id`),
+  CONSTRAINT `fk_cars_model` FOREIGN KEY (`modelID`) REFERENCES `models_car` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Дамп структуры для таблица car_catalog.favorites
+DROP TABLE IF EXISTS `favorites`;
+CREATE TABLE IF NOT EXISTS `favorites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL DEFAULT '0',
+  `carID` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_favorites_user` (`userID`),
+  KEY `fk_favorites_car` (`carID`),
+  CONSTRAINT `fk_favorites_car` FOREIGN KEY (`carID`) REFERENCES `cars` (`id`),
+  CONSTRAINT `fk_favorites_user` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
