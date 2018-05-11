113541af66330fbc74caeac734089d2f01280907
Pavel <pavel3423@gmail.com>
2018-05-11 21:17:14 +0300

    add main

--- src/main/java/by/htp/car_catalog/dao/hbn/Main.java
+++ src/main/java/by/htp/car_catalog/dao/hbn/Main.java
@@ -1,16 +1,16 @@
-//package by.htp.car_catalog.dao.hbn;
-//
-//import java.util.List;
-//
-//import by.htp.car_catalog.domain.Car;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//
-//	CarDaoHibernateImpl dao = new CarDaoHibernateImpl();
-//	List<Car> cars = dao.readByBrandAndModel("Audi", "A3 Sportback");
-//	System.out.println(cars);
-//    }
-//
-//}
+package by.htp.car_catalog.dao.hbn;
+
+import java.util.List;
+
+import by.htp.car_catalog.domain.Car;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	CarDaoHibernateImpl dao = new CarDaoHibernateImpl();
+	List<Car> cars = dao.readByBrandAndModel("Audi", "A3 Sportback");
+	System.out.println(cars);
+    }
+
+}
