d3c6977f00dc3c315255b062f0b3cfc945e891be
Pavel <pavel3423@gmail.com>
2018-05-15 19:41:21 +0300

    delete import

--- src/main/java/by/htp/car_catalog/service/impl/UserServiceImpl.java
+++ src/main/java/by/htp/car_catalog/service/impl/UserServiceImpl.java
@@ -5,7 +5,6 @@ import by.htp.car_catalog.domain.Role;
 import by.htp.car_catalog.domain.User;
 import by.htp.car_catalog.service.UserService;
 import by.htp.car_catalog.web.util.HttpRequestParamValidator;
-import by.htp.car_catalog.web.util.exception.runtimeException.ValidateNullObjectException;
 
 public class UserServiceImpl implements UserService {
 
