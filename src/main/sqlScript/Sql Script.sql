88c6799682ecf5431a1a3987f0f0194c9c4abe43
Pavel <pavel3423@gmail.com>
2018-05-30 13:22:36 +0300

    update sql script

--- src/main/sqlScript/Sql Script.sql
+++ src/main/sqlScript/Sql Script.sql
@@ -1,6 +1,5 @@
--- Дамп структуры базы данных car_catalog
 DROP DATABASE IF EXISTS `car_catalog`;
-CREATE DATABASE IF NOT EXISTS `car_catalog` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `car_catalog`
 USE `car_catalog`;
 
 -- Дамп структуры для таблица car_catalog.brands_car
@@ -8,11 +7,11 @@ DROP TABLE IF EXISTS `brands_car`;
 CREATE TABLE IF NOT EXISTS `brands_car` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `brand` varchar(50) NOT NULL,
-  `image` varchar(500) DEFAULT NULL,
+  `image` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `brand_UNIQUE` (`brand`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 -- Дамп структуры для таблица car_catalog.cars
 DROP TABLE IF EXISTS `cars`;
@@ -20,17 +19,26 @@ CREATE TABLE IF NOT EXISTS `cars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `brandID` int(11) NOT NULL DEFAULT '0',
   `modelID` int(11) NOT NULL DEFAULT '0',
-  `carClass` varchar(50) NOT NULL DEFAULT '0',
-  `equipment` varchar(50) NOT NULL,
-  `price` int(11) NOT NULL DEFAULT '0',
-  `image` varchar(500) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `bodyType` varchar(50) DEFAULT NULL,
+  `length` int(11) DEFAULT NULL,
+  `width` int(11) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `base` int(11) DEFAULT NULL,
+  `numberOfDoors` int(11) DEFAULT NULL,
+  `clearance` int(11) DEFAULT NULL,
+  `trunk` int(11) DEFAULT NULL,
+  `volumeOfTheTank` int(11) DEFAULT NULL,
+  `numberOfPlaces` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT '0',
+  `image` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`,`modelID`,`brandID`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_cars_brand_idx` (`modelID`),
   KEY `fk_cars_brand` (`brandID`),
   CONSTRAINT `fk_cars_brand` FOREIGN KEY (`brandID`) REFERENCES `models_car` (`brandID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_cars_model` FOREIGN KEY (`modelID`) REFERENCES `models_car` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- Дамп структуры для таблица car_catalog.favorites
 DROP TABLE IF EXISTS `favorites`;
@@ -42,8 +50,8 @@ CREATE TABLE IF NOT EXISTS `favorites` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_favorites_user` (`userID`),
   KEY `fk_favorites_car` (`carID`),
-  CONSTRAINT `fk_favorites_car` FOREIGN KEY (`carID`) REFERENCES `cars` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_favorites_user` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_favorites_car` FOREIGN KEY (`carID`) REFERENCES `cars` (`id`),
+  CONSTRAINT `fk_favorites_user` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Дамп структуры для таблица car_catalog.models_car
@@ -52,13 +60,12 @@ CREATE TABLE IF NOT EXISTS `models_car` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `brandID` int(11) NOT NULL DEFAULT '0',
   `model` varchar(50) NOT NULL,
-  `image` varchar(500) DEFAULT NULL,
+  `image` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`,`brandID`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `model_UNIQUE` (`model`),
   KEY `fk_models_brands` (`brandID`),
   CONSTRAINT `fk_models_brands` FOREIGN KEY (`brandID`) REFERENCES `brands_car` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
 
 -- Дамп структуры для таблица car_catalog.roles
 DROP TABLE IF EXISTS `roles`;
@@ -68,7 +75,7 @@ CREATE TABLE IF NOT EXISTS `roles` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `role_UNIQUE` (`role`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- Дамп структуры для таблица car_catalog.users
 DROP TABLE IF EXISTS `users`;
@@ -84,4 +91,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_users_roles_idx` (`roleID`),
   CONSTRAINT `fk_users_roles` FOREIGN KEY (`roleID`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
