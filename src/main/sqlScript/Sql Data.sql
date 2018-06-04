53be332dcca8ce0e4b83119ea4823c994f86486f
Pavel <pavel3423@gmail.com>
2018-06-04 22:57:34 +0300

    Update sql files

--- src/main/sqlScript/Sql Data.sql
+++ src/main/sqlScript/Sql Data.sql
@@ -9,7 +9,7 @@ INSERT INTO `brands_car` (`id`, `brand`, `image`) VALUES
 	(7, 'Honda', '4604633438716823481.png'),
 	(8, 'Hyundai', '4606707737298982482.png'),
 	(9, 'KIA', '4604806751984005397.png');
-
+	
 DELETE FROM `cars`;
 INSERT INTO `cars` (`id`, `modelID`, `year`, `bodyType`, `length`, `width`, `height`, `base`, `numberOfDoors`, `clearance`, `trunk`, `volumeOfTheTank`, `numberOfPlaces`, `price`, `image`) VALUES
 	(1, 1, 2018, 'Cabriolet', 4423, 1960, 1409, 2595, 2, 143, 320, 50, 4, 38350, '4604146090555141186.webp'),
@@ -30,15 +30,14 @@ INSERT INTO `models_car` (`id`, `brandID`, `model`, `image`) VALUES
 	(5, 2, '3 series', '4600559156716143948.png'),
 	(6, 2, 'X5', '4604290594292120066.png');
 
-
 DELETE FROM `roles`;
 INSERT INTO `roles` (`id`, `role`) VALUES
 	(1, 'admin'),
 	(3, 'moderator'),
 	(2, 'user');
-
+	
 DELETE FROM `users`;
-INSERT INTO `users` (`id`, `login`, `email`, `password`, `roleID`) VALUES
-	(1, 'admin', 'admin@gmail.com', 'admin', 1),
-	(2, 'user', 'user@gmail.comm', '', 2),
-	(3, 'moderator', 'moderator', 'moderator', 3);
+INSERT INTO `users` (`id`, `login`, `email`, `password`, `salt`, `roleID`) VALUES
+	(1, 'admin', 'admin@admin.com', 'afGE+NeY5CckOwPuZRQkdCDeNalTCuUQXBy5YgdiZWB60RV80ze8sea/do88FWuM8ifQrDVW14cFJ7DOkSAHvtmDlw0UrGmENDQkPE9fudo=', 'c15ac68b4cf1ba92d0aeb18b67f2da46e49ee1f64daee65f13050e05f69933b648c099190e905af719ff258ef2d356d0878d9e2bdedf8e7b047036fe9df196f2', 1),
+	(2, 'moderator', 'moderator@moderator.com', 'DQe+6/wpQIZXeZi2WjoJXm+Lw5D+CNrH2Yp8oxBYnoH6n5cjLwzkr2LYKOfIlPM3aiuBszQeYkHNsr60mSWD0T+Kh1T3AK1OVXfY1+TcbLY=', 'dbeb038d2b1afc8b6a1dfbfa78d8f398112e6793d04d93124af1fb78c1f3a94d721642069bc8766dc0c895e0d6f4b5cf648214effb63b0a02ad1eb311274e8f8', 3),
+	(3, 'user', 'user@user.com', '+173gHgReQXV/k2CGkMv2754pEV6+IEP0irjM+ZiJ+801gg6mxiL28sOMU/LoxUVqGcdoSmVYYORtGuPER1t/V6EfBH55MuMX09mPhthw+A=', '791cf3cd17e855b63c2a3a10a1decd96e5861f038544e28e154c3d00ba534ffb8bd22f6ca2cabca8f4df1e765c311775546df1705761bbd260fbf089182ca8d2', 2);
--- src/main/sqlScript/Sql Script.sql
+++ src/main/sqlScript/Sql Script.sql
@@ -1,8 +1,7 @@
 DROP DATABASE IF EXISTS `car_catalog`;
-CREATE DATABASE IF NOT EXISTS `car_catalog`
+CREATE DATABASE IF NOT EXISTS `car_catalog`;
 USE `car_catalog`;
 
--- Дамп структуры для таблица car_catalog.brands_car
 DROP TABLE IF EXISTS `brands_car`;
 CREATE TABLE IF NOT EXISTS `brands_car` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -11,9 +10,8 @@ CREATE TABLE IF NOT EXISTS `brands_car` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `brand_UNIQUE` (`brand`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
--- Дамп структуры для таблица car_catalog.cars
 DROP TABLE IF EXISTS `cars`;
 CREATE TABLE IF NOT EXISTS `cars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,11 +31,11 @@ CREATE TABLE IF NOT EXISTS `cars` (
   `image` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`,`modelID`),
   UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `modelID` (`modelID`),
   KEY `fk_cars_brand_idx` (`modelID`),
   CONSTRAINT `fk_cars_model` FOREIGN KEY (`modelID`) REFERENCES `models_car` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
--- Дамп структуры для таблица car_catalog.favorites
 DROP TABLE IF EXISTS `favorites`;
 CREATE TABLE IF NOT EXISTS `favorites` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -51,7 +49,6 @@ CREATE TABLE IF NOT EXISTS `favorites` (
   CONSTRAINT `fk_favorites_user` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп структуры для таблица car_catalog.models_car
 DROP TABLE IF EXISTS `models_car`;
 CREATE TABLE IF NOT EXISTS `models_car` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -62,9 +59,8 @@ CREATE TABLE IF NOT EXISTS `models_car` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_models_brands` (`brandID`),
   CONSTRAINT `fk_models_brands` FOREIGN KEY (`brandID`) REFERENCES `brands_car` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
--- Дамп структуры для таблица car_catalog.roles
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE IF NOT EXISTS `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -74,13 +70,13 @@ CREATE TABLE IF NOT EXISTS `roles` (
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Дамп структуры для таблица car_catalog.users
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `salt` varchar(128) NOT NULL,
   `roleID` int(11) NOT NULL,
   PRIMARY KEY (`id`,`roleID`),
   UNIQUE KEY `login` (`login`),
@@ -88,4 +84,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_users_roles_idx` (`roleID`),
   CONSTRAINT `fk_users_roles` FOREIGN KEY (`roleID`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
